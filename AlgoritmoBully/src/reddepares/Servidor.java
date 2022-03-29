4e8385fdc70cef81e53b5ba73711264d1c130ac9
diegomolinai <diegito_mr_pc@hotmail.com>
2022-03-29 18:16:44 -0300

    Actualizacion V2

--- AlgoritmoBully/src/reddepares/Servidor.java
+++ AlgoritmoBully/src/reddepares/Servidor.java
@@ -10,13 +10,13 @@ public class Servidor {
         ServerSocket serverSocket = null;
         Socket socket = null;
         String peticion = null;
-        String id = "1";
-        String[] ips = {"26.157.199.222","26.187.118.118","26.157.200.26","26.25.191.84"};
-        String[] ipMayor = {"26.25.191.84","26.157.200.26","26.187.118.118","26.157.199.222"};
+        String id = "6";
+        String[] ips = {"26.187.118.118", "26.157.199.222", "26.25.191.84", "26.157.200.26"};
+        String[] ipMayor = {"26.187.118.118"};
         String ip = "";
-        int numeroAzar = (int)(Math.random()*100 + 1);
-        int numeroAzar2 = (int)(Math.random()*100 + 1);
-        Hilo Hilo = new Hilo(" " + numeroAzar ,ips);  
+        int numeroAzar = (int) (Math.random() * 100 + 1);
+        int numeroAzar2 = (int) (Math.random() * 100 + 1);
+        Hilo Hilo = new Hilo("Algo", ips);
         Hilo.start();
 
         try {
@@ -32,7 +32,21 @@ public class Servidor {
             if (Hilo.buscarCoordinador == false) {
                 try {
                     socket = serverSocket.accept();
-                    System.out.println("Se conecto un cliente: " + socket.getInetAddress().getHostName());
+                    String ipRecivida = "" + socket.getInetAddress();
+
+                    if (ipRecivida.equals("/26.157.199.222")) {
+                        System.out.println("Se conecto un cliente: " + "MatiasPC");
+                    }
+                    if (ipRecivida.equals("/26.157.200.26")) {
+                        System.out.println("Se conecto un cliente: " + "DiegoPC");
+                    }
+                    if (ipRecivida.equals("/26.187.118.118")) {
+                        System.out.println("Se conecto un cliente: " + "DiegoVM");
+                    }
+                    if (ipRecivida.equals("/26.25.191.84")) {
+                        System.out.println("Se conecto un cliente: " + "MatiasVM");
+                    }
+                    //System.out.println("Se conecto un cliente: " + socket.getInetAddress().getHostName());
                     DataInputStream dis = new DataInputStream(socket.getInputStream());
                     peticion = dis.readUTF();
                     if (!peticion.equals("Algo")) {
@@ -44,21 +58,17 @@ public class Servidor {
                             if (peticion.split(": ")[0].equals("El coordinador es")) {
                                 System.out.println("------------" + peticion + "------------" + "Ha finalizado la busqueda de coordinador");
                                 Hilo.buscarCoordinador = false;
-
                             }
                         }
                     } else {
                         System.out.println("El mensaje que me envió el cliente es: " + peticion);
-
                     }
-
                     dis.close();
                     socket = null;
                 } catch (IOException e) {
                     System.out.println("java.io.IOException generada");
                     e.printStackTrace();
                 }
-
             } else {
                 if (ipMayor.length == 0) {
                     Hilo.enviarMensaje("El coordinador es: " + id);
@@ -67,7 +77,6 @@ public class Servidor {
                     for (int i = 0; i < clienteMayor.length; i++) {
                         clienteMayor[i] = new Hilo(ipMayor[i]);
                         clienteMayor[i].start();
-
                     }
                     boolean encontrado = false;
 
@@ -93,4 +102,4 @@ public class Servidor {
         }
 
     }
-} 
+}
